7c3302720c1d13500ba6b3288624121725115ea7
Melanie Ng <melanienps@gmail.com>
2021-05-06 11:10:23 +0800

    Send password reset email to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
 
@@ -59,6 +59,18 @@ class User < ApplicationRecord
         UserMailer.account_activation(self).deliver_now
     end
 
+    #Creates and assigns password reset token and digest
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:remember_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    #Sends password reset email
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+
     private
         #Converts user email to lower case
         def downcase_email
@@ -70,5 +82,4 @@ class User < ApplicationRecord
             self.activation_token = User.new_token
             self.activation_digest = User.digest(activation_token)
         end
-    
 end
